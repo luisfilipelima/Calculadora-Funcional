5c2dde395b4345a7a4eb0e60a57941ce247ebea5
Luís Lima <luisf4097@hotmail.com>
2016-03-11 12:45:48 -0300

    Implementação das Operações Lógicas
    Implementando OU e E lógicos.

--- avaliacao.ml
+++ avaliacao.ml
@@ -12,13 +12,14 @@ let rec avalia memoria exp =
                    let (valor_y, memoria'') = avalia memoria' y in
                    let valor_res =
                      match op with
-                     | Som -> valor_x +. valor_y
-                     | Sub -> valor_x -. valor_y
-                     | Pro -> valor_x *. valor_y
-                     | Div -> valor_x /. valor_y
-                     | Pot -> valor_x ** valor_y
+                     | Som      -> valor_x +. valor_y
+                     | Sub      -> valor_x -. valor_y
+                     | Pro      -> valor_x *. valor_y
+                     | Div      -> valor_x /. valor_y
+                     | Pot      -> valor_x ** valor_y
+                     | ELog     -> if valor_x != 0 && valor_y != 0 then 1. else 0.
+                     | OuLog    -> if valor_x != 0 || valor_y != 0 then 1. else 0.
                    in
                    (valor_res, memoria'')
   | Atr (nome, x) -> let (valor_x, memoria') = avalia memoria x in
                      (valor_x, (nome, valor_x)::memoria')
-                     
