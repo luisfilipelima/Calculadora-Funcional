38a9b246362a7ff3f71fc2b4526f7426637837f0
Luis F. Lima <luisflavieira@gmail.com>
2016-03-15 01:32:15 -0300

    Implementação do Padrao de ExCon

--- avaliacao.ml
+++ avaliacao.ml
@@ -25,9 +25,20 @@ let rec avalia memoria exp =
             | MirIgd   -> if valor_x >= valor_y then 1. else 0.
             | Mnr      -> if valor_x < valor_y then 1. else 0.
             | MnrIgd   -> if valor_x <= valor_y then 1. else 0.
+            (* | If       ->
+            | Then     -> avalia memoria x
+            | Else     -> avalia memoria x *)
         in
         ( valor_res, memoria'' )
     | NegaOp ( op, x ) -> let ( valor_x, memoria' ) = avalia memoria x in
         if valor_x = 0. then ( 1., memoria' ) else ( 0., memoria' )
     | Atr ( nome, x ) -> let ( valor_x, memoria' ) = avalia memoria x in
         ( valor_x, ( nome, valor_x ) :: memoria' )
+    | ExCon ( s1, e1, s2, e2, s3, e3 ) ->
+        let ( resultE1, e1' ) = avalia memoria e1 in
+        if resultE1 != 0. then
+            let ( resultE2, e2' ) = avalia memoria e2 in
+            ( resultE2, e2' )
+        else
+            let ( resultE3, e3' ) = avalia memoria e3 in
+            ( resultE3, e3' )
